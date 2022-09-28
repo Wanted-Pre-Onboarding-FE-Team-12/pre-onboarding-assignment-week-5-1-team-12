929c19c8f09aa815d2d128c57d4077d26979138a
KiHoon Roh <started677@gmail.com>
2022-09-29 01:30:41 +0900

    fix: redux-logger 출력 오류 삭제

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import searchSlice from './searchSlice';
 
 export const store = configureStore({
   reducer: { search: searchSlice },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
   devTools: false,
 });
